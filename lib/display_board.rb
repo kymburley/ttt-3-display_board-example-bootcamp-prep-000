e7910d3d1e559186a08ee9b64ca5bd113a57a23d
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-10 13:09:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   cell = "   "
+  cell_separator = "|"
+
+  puts "#{cell}#{cell_separator}#{cell}#{cell_separator}#{cell}"
 end
